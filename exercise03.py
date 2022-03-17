f5f1ae3c0ba09d748afa833f4de19758b77783c7
ihtiyare <38437070+ihtiyare@users.noreply.github.com>
2022-03-17 19:26:34 +0300

    Update and rename python.txt to exercise03.py

---
+++ exercise03.py
@@ -0,0 +1,10 @@
+#this is a program which accept the radius of a circle from user and compute the area.
+#from math import pi //if you activate this line you shouldn't define the pi. You can use directly pi when you calculating anything.
+def areaOfCircle(radius):
+	pi=3.14
+	return pi*radius*radius
+	#we use radius**2 for the two to the power of radius.
+
+r=float(input("Enter the radius of circle: "))
+area=areaOfCircle(r)
+print("The area of the circle is", area)

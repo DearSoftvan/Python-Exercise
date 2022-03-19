96b7bf505458dcbc4fe0f0e5053fc63c039d6123
ihtiyare <38437070+ihtiyare@users.noreply.github.com>
2022-03-19 17:46:25 +0300

    Update exercise04.py

--- exercise04.py
+++ exercise04.py
@@ -1,12 +1,14 @@
 #This is a program that accepts an integer (n) and computes the value of n+nn+nnn
 #for instance if n=5 > 5+55+555
-def program(n):
+def program(n,time):
     carrier=n
     total=carrier
-    for i in range (2):
+    for i in range (time-1):
         carrier=carrier*10
         total=total+carrier
     return total
 
-result=program(5)
+result=program(5,3)
 print(result)
+
+

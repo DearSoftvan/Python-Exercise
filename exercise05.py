d429d345423b04cb2aa81dafca0fef97945ff70f
ihtiyare <38437070+ihtiyare@users.noreply.github.com>
2022-03-23 12:17:38 +0300

    Update exercise05.py

--- exercise05.py
+++ exercise05.py
@@ -2,8 +2,10 @@ def dictMax(dictionary, keys):
 	numberOfKeys=len(keys)
 	values= []
 	for i in range (numberOfKeys):
-	    if keys[i] in dictionary:
+	   if keys[i] in dictionary:
 	        values.append(dictionary[keys[i]])
+	   else:
+	       print("There is ",keys[i]," in key list, but there aren't any key in the dictionary like as ", keys[i],".")
 	    #values.append(dictionary[keys[i]])
 	    #print("key: ", keys[i], "Value: ",dictionary[keys[i]])
 	return max(values)

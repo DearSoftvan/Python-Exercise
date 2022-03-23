1f6fec0a67e62112a3e1fe9b10fb27cf1e6f0a4d
DearSoftvan <38437070+DearSoftvan@users.noreply.github.com>
2022-03-23 16:13:04 +0300

    Create power.py

---
+++ power.py
@@ -0,0 +1,10 @@
+#This is a program for calculate a to the power of n.
+
+def power(a, n):
+    result=a
+    for i in range(n-1):
+        result=result*a
+    return result
+
+result=power(5, 2)
+print(result)

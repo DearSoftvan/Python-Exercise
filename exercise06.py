20f4a7a4949bbd8e5121df8163aab2c5f96ec96a
DearSoftvan <38437070+DearSoftvan@users.noreply.github.com>
2022-03-31 20:44:19 +0300

    Update exercise06.py

--- exercise06.py
+++ exercise06.py
@@ -2,7 +2,10 @@
 #Write a Python program to filter the positive numbers from a list.
 
 def filter(list):
-    
+    for i in list:
+        if(int(list[i]) > 0):
+            return 155
     return list
 
-my_list = [1, "Hello", 3.4]
+my_list = [1, 7 , 3.4]
+filter(my_list)
